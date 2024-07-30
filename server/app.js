1c4d23ccc1c3f094f71ddecfdb17a253e03fa2da
Polina Yukhymenko <polya.kiev@gmail.com>
2024-07-30 12:27:21 +0200

    day1

--- server/app.js
+++ server/app.js
@@ -6,11 +6,14 @@ const PORT = 5005;
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
+const cohorts = require("./cohorts.json");
 
+const students = require("./students.json");
+
+const cors = require("cors");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -21,14 +24,23 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
+app.use(
+  cors({
+    origin: "http://localhost:5005", // Replace with your frontend URL
+  })
+);
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
-
+app.get("api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
+app.get("api/students", (req, res) => {
+  res.json(students);
+});
 
 // START SERVER
 app.listen(PORT, () => {
